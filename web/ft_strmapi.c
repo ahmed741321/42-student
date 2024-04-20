2b593c9b052de7efdaf998027687cdab59668211
AHMET ASAAD HAMMOUD <88238215+ahmed741321@users.noreply.github.com>
2024-04-20 11:46:52 +0200

    Add files via upload

---
+++ web/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasaad-h <aasaad-h@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/07 21:58:53 by hbaddrul          #+#    #+#             */
+/*   Updated: 2024/04/11 16:16:59 by aasaad-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char			*ret;
+	unsigned int	i;
+
+	if (!s)
+		return (0);
+	ret = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!ret)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = f(i, s[i]);
+		++i;
+	}
+	ret[i] = 0;
+	return (ret);
+}
