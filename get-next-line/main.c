7f6e8434617b2ac394c06bce9283f63c55dbf932
Ahmet Asaad Hammoud <88238215+ahmed741321@users.noreply.github.com>
2024-05-26 17:40:21 +0200

    Add files via upload

---
+++ get-next-line/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasaad-h < aasaad-h@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 17:23:43 by aasaad-h          #+#    #+#             */
+/*   Updated: 2024/05/26 17:28:46 by aasaad-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	int fd;
+
+	fd = open("prueba.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	return (0);
+}
